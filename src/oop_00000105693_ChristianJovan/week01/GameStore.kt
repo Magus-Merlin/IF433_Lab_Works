d3bbadf16c570d63149b1b211f97c8f08c1097d3
Magus-Merlin <chrisitanjovan06@gmail.com>
2026-02-02 16:38:59 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000105693_ChristianJovan/week01/GameStore.kt
+++ src/oop_00000105693_ChristianJovan/week01/GameStore.kt
@@ -4,4 +4,17 @@ fun main() {
     val gameTitle = "Persona 4 Revival"
     val price = 699000
 
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
 }
+
+fun calculateDiscount(price: Int) : Int {
+    return when(price){
+        in 0..50000 -> 0
+        in 50001..100000 -> 10000
+        in 100001..300000 -> 30000
+        in 300001..600000 -> 60000
+        else -> 90000
+    }
+}
+
