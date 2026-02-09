d3669e23467fe6d474ef67d676df97e85b9d3d34
Magus-Merlin <chrisitanjovan06@gmail.com>
2026-02-09 15:27:18 +0700

    week02: (task 2) implement the game

--- src/oop_00000105693_ChristianJovan/week02/Main.kt
+++ src/oop_00000105693_ChristianJovan/week02/Main.kt
@@ -41,26 +41,87 @@ fun main() {
         println("Pilihan ngawur, pendaftaran batal!")
     }
 
-    print("Masukkan Judul Buku: ")
-    val bookTitle = scanner.nextLine()
-
-    print("Masukkan Nama Peminjam: ")
-    val borrower = scanner.nextLine()
-
-    print("Masukkan Durasi Peminjaman (Hari): ")
-    var loanDuration = scanner.nextInt()
-
+    print("Pilih aplikasi yang ingin digunkan (1.PMB UMN, 2.UMN RPG): ")
+    val app = scanner.nextInt()
     scanner.nextLine()
 
-    if (loanDuration < 0){
-        loanDuration = 1
+    if (app == 1){
+        print("Masukkan Judul Buku: ")
+        val bookTitle = scanner.nextLine()
+
+        print("Masukkan Nama Peminjam: ")
+        val borrower = scanner.nextLine()
+
+        print("Masukkan Durasi Peminjaman (Hari): ")
+        var loanDuration = scanner.nextInt()
+
+        scanner.nextLine()
+
+        if (loanDuration < 0){
+            loanDuration = 1
+        }
+
+        val loan = Loan(bookTitle, borrower, loanDuration)
+
+        println("--- DETAIL PEMINJAMAN ---")
+        println("Judul Buku : ${loan.bookTitle}")
+        println("Peminjaman : ${loan.borrower}")
+        println("Durasi Peminjaman : ${loan.loanDuration}")
+        println("Denda : ${loan.calculateFine()}")
+
+    }else if(app == 2){
+
+        print("Masukkan nama Hero   : ")
+        val heroName = scanner.nextLine()
+
+        print("Masukkan damage Hero : ")
+        val baseDamage  = scanner.nextInt()
+
+        val enemy = "Minotaur"
+        var enemyHP = 120
+
+        val hero = Hero(name = heroName, baseDamage = baseDamage)
+
+        println("Enemy $enemy appeared!")
+        while(hero.isAlive() && enemyHP > 0){
+            println("1. Attack")
+            println("2. Run")
+            print("Choose: ")
+            val choose = scanner.nextInt()
+
+            if (choose == 1){
+                hero.attack(targetName = enemy)
+                val critical = (1..5).random()
+                if (critical == 3) {
+                    enemyHP -= baseDamage * 2
+                    println("CRITICAL HIT!")
+                }else{
+                    enemyHP -= baseDamage
+                }
+                if (enemyHP > 0) {
+                    println("$enemy remaining HP = $enemyHP")
+                    println("The enemy counter attacks!")
+                    hero.takeDamage((10..20).random())
+                    println("Your remaining HP = ${hero.hp}")
+                    if (hero.hp == 0){
+                        println("You lose..")
+                        println("Thanks for playing the game!")
+                        break
+                    }
+                } else{
+                    println("Congratulations, you have defeated the $enemy!")
+                }
+            }
+            else if (choose == 2){
+                println("You successfully ran from the battle!")
+                break
+            }else {
+                println("Choose the option provided!")
+            }
+        }
+
+    }else {
+        println("Terimakasih!")
     }
 
-    val loan = Loan(bookTitle, borrower, loanDuration)
-
-    println("--- DETAIL PEMINJAMAN ---")
-    println("Judul Buku : ${loan.bookTitle}")
-    println("Peminjaman : ${loan.borrower}")
-    println("Durasi Peminjaman : ${loan.loanDuration}")
-    println("Denda : ${loan.calculateFine()}")
 }
