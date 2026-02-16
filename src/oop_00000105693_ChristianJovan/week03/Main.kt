02481e3f27cfcd4943a346a95274dc5ff1cc44b6
Magus-Merlin <chrisitanjovan06@gmail.com>
2026-02-16 20:52:07 +0700

    week03: cleanup code

--- src/oop_00000105693_ChristianJovan/week03/Main.kt
+++ src/oop_00000105693_ChristianJovan/week03/Main.kt
@@ -3,15 +3,11 @@ package oop_00000105693_ChristianJovan.week03
 fun main() {
     val e = Employee("Budi")
 
-    //1. Test Validasi Salary
     e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
-    //2. Test Encapsulation
     e.increasePerformance()
-    // e.perfomanceRating = 5
 
-    //3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
