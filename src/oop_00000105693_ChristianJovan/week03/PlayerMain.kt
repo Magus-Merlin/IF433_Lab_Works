bd16d637064812646be94847b38a4e3f7a568735
Magus-Merlin <chrisitanjovan06@gmail.com>
2026-02-16 21:35:43 +0700

    week03: (task 2) implement Player testing in main

---
+++ src/oop_00000105693_ChristianJovan/week03/PlayerMain.kt
@@ -0,0 +1,8 @@
+package oop_00000105693_ChristianJovan.week03
+
+fun main(){
+    val p = Player("Arthur")
+    //p.xp = 10 - Pasti error karena xp itu private property
+    p.addXP(50)
+    p.addXP(60)
+}
