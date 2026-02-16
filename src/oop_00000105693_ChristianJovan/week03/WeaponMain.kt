d2d1b3f09ecdc7ef9e042ac7040c8b1948b699e4
Magus-Merlin <chrisitanjovan06@gmail.com>
2026-02-16 21:13:14 +0700

    week03: (task 1) implement Weapon testing in main

---
+++ src/oop_00000105693_ChristianJovan/week03/WeaponMain.kt
@@ -0,0 +1,8 @@
+package oop_00000105693_ChristianJovan.week03
+
+fun main() {
+    val w = Weapon("Excalibur")
+    w.damage = -50
+    w.damage = 9999
+    println("${w.name} is ${w.tier} tier weapon!")
+}
