b8b871065d44297c9ce1c439e466fe77466b565f
Magus-Merlin <chrisitanjovan06@gmail.com>
2026-03-09 15:58:52 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000105693_ChristianJovan/week06/TugasMain.kt
+++ src/oop_00000105693_ChristianJovan/week06/TugasMain.kt
@@ -5,5 +5,16 @@ fun main(){
     val smartSpeaker = SmartSpeaker("Google Nest Dapur", "SS001")
     val smartCCTV = SmartCCTV("Ezviz Garasi", "SCCTV001")
 
+    val hub = SmartHomeHub()
 
+    println("=== MENAMBAHKAN DEVICE ===")
+    hub.addDevice(smartLamp)
+    hub.addDevice(smartSpeaker)
+    hub.addDevice(smartCCTV)
+
+    println("\n=== MENYALAKAN SECURITY ===")
+    hub.activateSecurityMode()
+
+    println("\n=== MEMATIKAN DEVICE ===")
+    hub.turnOffAllSwitches()
 }
