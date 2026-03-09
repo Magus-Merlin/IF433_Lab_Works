bcb1a66ccc111995422ce841a88f9443a9e434ff
Magus-Merlin <chrisitanjovan06@gmail.com>
2026-03-09 15:05:54 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_00000105693_ChristianJovan/week06/SmartInterfaces.kt
@@ -0,0 +1,21 @@
+package oop_00000105693_ChristianJovan.week06
+
+interface SmartDevice {
+    val id : String
+
+    val name: String
+}
+
+interface Switchable{
+    fun turnOn()
+
+    fun turnOff()
+}
+
+interface Recordable{
+    fun startRecord()
+
+    fun stopRecord(){
+        println("Perekaman dihentikan dan disimpan ke Cloud.")
+    }
+}
