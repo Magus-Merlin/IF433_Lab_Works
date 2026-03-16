620c1a0703f461c91b71fe5a2647c4b95b1241f0
Magus-Merlin <chrisitanjovan06@gmail.com>
2026-03-16 14:52:31 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_00000105693_ChristianJovan/week07/GameManager.kt
@@ -0,0 +1,15 @@
+package oop_00000105693_ChristianJovan.week07
+
+object GameManager{
+    var isGameRunning: Boolean = false
+
+    fun startGame(){
+        if(isGameRunning){
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        }else{
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+
+}
