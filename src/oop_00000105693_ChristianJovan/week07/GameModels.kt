4c9b4674e7bb0f42b9c12ba1cbeb03d419dce97d
Magus-Merlin <chrisitanjovan06@gmail.com>
2026-03-16 15:01:08 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000105693_ChristianJovan/week07/GameModels.kt
@@ -0,0 +1,11 @@
+package oop_00000105693_ChristianJovan.week07
+
+enum class ItemRarity (val dropChance: Int){
+    COMMON(40),
+    UNCOMMON(30),
+    RARE(15),
+    EPIC(10),
+    LEGENDARY(5)
+}
+
+
