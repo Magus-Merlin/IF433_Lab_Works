ccfcfea1551d9aeb436fa7d8a58603d7a514246e
Magus-Merlin <chrisitanjovan06@gmail.com>
2026-03-16 13:14:35 +0700

    week07: implement companion object as factory in NetworkClient

--- src/oop_00000105693_ChristianJovan/week07/NetworkClient.kt
+++ src/oop_00000105693_ChristianJovan/week07/NetworkClient.kt
@@ -1,6 +1,16 @@
 package oop_00000105693_ChristianJovan.week07
 
 class NetworkClient private constructor(var url: String) {
+
+    companion object {
+        const val BASE_URL = "https://api.umn.ac.id"
+
+        fun createClient(): NetworkClient {
+            println("Membangung NetworkClient dengan BASE_URL: $BASE_URL")
+            return NetworkClient(BASE_URL)
+        }
+    }
+
     fun connect(){
         println("Connecting to $url...")
     }
