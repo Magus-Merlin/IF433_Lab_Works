fae914a02a13a7a755a723d4cc66656569805965
Magus-Merlin <chrisitanjovan06@gmail.com>
2026-03-16 16:20:19 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_00000105693_ChristianJovan/week07/MainGame.kt
+++ src/oop_00000105693_ChristianJovan/week07/MainGame.kt
@@ -6,10 +6,34 @@ fun main(){
 
     println("--- TIP! Legendary weapon has a ${ItemRarity.LEGENDARY.dropChance}% drop rate! ---")
 
-    val StarterSword = Weapon.forgeStarterSword()
+    val starterSword = Weapon.forgeStarterSword()
     println("You got a weapon to start your journey!")
-    println("- Name       : ${StarterSword.item.name}")
-    println("- Durability : ${StarterSword.durability}")
-    println("- Damage     : ${StarterSword.item.damage}")
-    println("- Rarity     : ${StarterSword.item.rarity}")
+    println("- Name       : ${starterSword.item.name}")
+    println("- Durability : ${starterSword.durability}")
+    println("- Damage     : ${starterSword.item.damage}")
+    println("- Rarity     : ${starterSword.item.rarity}")
+
+    println("Would you like to go to the forgery?(Press 1 to continue)")
+    val choice = readln()
+
+    val itemToLoot = starterSword.item
+
+    when (choice) {
+        "1"->{
+            println("Welcome to the forgery!")
+            println("As a gift, I will upgrade your sword!")
+
+            val upgradedItem =  starterSword.item.copy(name = "Enhanced Sword", damage = 25)
+
+            println("=== REFORGE SUCCESS! ===")
+            println("- Name       : ${upgradedItem.name}")
+            println("- Damage     : ${upgradedItem.damage}")
+        }
+        else -> return
+    }
+
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+    processEvent(BattleState.LootDropped(itemToLoot))
+    processEvent(BattleState.GameOver("Terkena jebakan racun!"))
 }
