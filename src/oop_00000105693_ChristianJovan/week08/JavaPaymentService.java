8ac332ae6b34770acc609755008b7eed82453dc0
Magus-Merlin <chrisitanjovan06@gmail.com>
2026-04-13 14:59:31 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000105693_ChristianJovan/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000105693_ChristianJovan.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId){
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
