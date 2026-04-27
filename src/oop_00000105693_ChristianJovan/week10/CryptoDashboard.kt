a2ea184894caf464bd04b1e251b83392ca56d7df
Magus-Merlin <chrisitanjovan06@gmail.com>
2026-04-27 13:34:57 +0700

    week10: (task) populate coin repository

--- src/oop_00000105693_ChristianJovan/week10/CryptoDashboard.kt
+++ src/oop_00000105693_ChristianJovan/week10/CryptoDashboard.kt
@@ -3,4 +3,9 @@ package oop_00000105693_ChristianJovan.week10
 fun main(){
     val coinRepo = WalletRepository<Coin>()
 
+    coinRepo.add(Coin("BTC", 75000.0))
+    coinRepo.add(Coin("ETH", 55000.0))
+    coinRepo.add(Coin("USDT", 65000.0))
+
+
 }
