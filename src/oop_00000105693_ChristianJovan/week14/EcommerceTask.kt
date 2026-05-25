db9d0212d3b2903933e7abca36aeb4f7e93735d5
Magus-Merlin <chrisitanjovan06@gmail.com>
2026-05-25 16:01:16 +0700

    week14: (task) implement PricingStrategy interface for OCP

--- src/oop_00000105693_ChristianJovan/week14/EcommerceTask.kt
+++ src/oop_00000105693_ChristianJovan/week14/EcommerceTask.kt
@@ -45,23 +45,49 @@ class EmailNotifier : NotificationService{
     }
 }
 
+interface PricingStrategy{
+    fun calculate(price:Double):Double
+}
+
+class RegularPricing(val customerType: String ="regular") : PricingStrategy {
+    override fun calculate(price: Double): Double = price
+}
+
+class VipPricing(val customerType: String ="vip") : PricingStrategy {
+    override fun calculate(price: Double): Double = price * 0.90
+}
+
+
 class SafeOrderProcessor(
     private val repo: OrderRepository,
     private val notifier: NotificationService
 ){
-    fun processOrders(itemName: String, basePrice: Double, customerType: String){
-        val finalPrice = when(customerType){
-            "regular" -> basePrice
-            "vip" -> basePrice * 0.90
-            else -> basePrice
-        }
+    fun processOrders(itemName: String, basePrice: Double, pricingStrategy: PricingStrategy, typeLabel: String) {
+        val finalPrice = pricingStrategy.calculate(basePrice)
 
         println("Memproses Pesanan $itemName seharga $finalPrice")
 
-        repo.saveOrder(itemName, finalPrice, customerType)
+        repo.saveOrder(itemName, finalPrice, typeLabel)
 
         notifier.sendNotification(itemName)
 
-        println("--- Pesanan Selesai Diproses ---")
+        println("--- Pesanan Selesai Diproses ---\n")
     }
+}
+
+//Buat tes hasil
+fun main() {
+    val repo = CsvOrderRepository("orders.csv")
+    val notifier = EmailNotifier()
+    val orderProcessor = SafeOrderProcessor(repo, notifier)
+
+    println("=== SIMULASI EKSEKUSI SOLID ===")
+
+    // Transaksi 1: Menggunakan strategi VIP
+    val vipStrategy = VipPricing()
+    orderProcessor.processOrders("Monitor ASUS", 3000000.0, vipStrategy, vipStrategy.customerType)
+
+    // Transaksi 2: Menggunakan strategi Regular
+    val regularStrategy = RegularPricing()
+    orderProcessor.processOrders("Keyboard Mechanical", 800000.0, regularStrategy, regularStrategy.customerType)
 }
